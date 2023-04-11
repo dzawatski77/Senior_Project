f84c1c60b5362671aa5dc3b55b261d3004a9a5ed
dzawatski77 <dmz05520@sjfc.edu>
2023-04-11 13:42:38 -0400

    Update CreateEvent.jsx

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -5,90 +5,36 @@ import { useStateContext } from '../contexts/ContextProvider';
 import ReactDOM from "react-dom";
 import TimePicker from 'react-time-picker';
 import { Link } from "react-router-dom";
-//import Event from "../components/Event";
 
 
 
 
   const CreateEvent = () => {
     const { currentColor } = useStateContext();
-    const [timeStart, onChange1] = useState('10:00');
-    const [timeEnd, onChange2] = useState('11:00');
-    const [errorMessages, setErrorMessages] = useState({});
-    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [eventName, setEventName] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleEventNameChange = (e) => {
+    setEventName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (eventName.trim() === "") {
+      setErrorMessage("Please enter an event name.");
+    } else {
+      // Handle event submission logic here
+    }
+  };
 
   return (
     <div className=' m-2 md:m-10 p-2 md:p-10 drop-shadow-2xl dark:text-gray-200 dark:bg-main-dark-bg bg-white rounded-3xl'>
       <Header category="Page" title="Create Event" />
       <main className='mt-8 max-w-md'>
-        <form className='grid grid-cols-1 gap-6'>
-          <label class="eventName">
-            <span class="text-gray-700 dark:text-gray-200">Event Name:</span>
-            <input type="text" class="
-              p-2
-              mt-1 
-              block 
-              w-full 
-              rounded-md
-              drop-shadow-md
-             dark:text-gray-700
-             bg-gray-200
-             border-gray-300
-             focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder></input>
-          </label>
-
-          <label class="eventDate">
-            <span class="text-gray-700 dark:text-gray-200">When is your Event?</span>
-            <input type="date" class="
-              p-2
-              mt-1
-              block
-              w-full
-              drop-shadow-md
-              rounded-md
-              dark:text-gray-700
-              bg-gray-200
-              border-gray-300
-              focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder="mm/dd/yyyy"></input>
-          </label>
-
-          <label class="eventStartTime">
-            <span class="text-gray-700 dark:text-gray-200">What time does your event Start?</span>
-            <input type="time" class="
-              p-2
-              mt-1
-              block
-              w-full
-              drop-shadow-md
-              rounded-md
-              dark:text-gray-700
-              bg-gray-200
-              border-gray-300
-              focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder=''></input>
-          </label>
-
-          <label class="eventEndTime">
-            <span class="text-gray-700 dark:text-gray-200">What time does your event end?</span>
-            <input type="time" class="
-              p-2
-              mt-1
-              block
-              w-full
-              rounded-md
-              drop-shadow-md
-              bg-gray-200
-              border-gray-300
-              dark:text-gray-700
-              focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder='Event Name'></input>
-          </label>
-
-          <label class="eventName">
-            <span class="text-gray-700 dark:text-gray-200">Where will your event be held?</span>
-            <input type="text" class="
+        <form className='grid grid-cols-1 gap-6' onSubmit={handleSubmit}>
+          <label className="eventName">
+            <span className="text-gray-700 dark:text-gray-200">Event Name:</span>
+            <input type="text" className="
               p-2
               mt-1
               block
@@ -99,12 +45,64 @@ import { Link } from "react-router-dom";
               bg-gray-200
               border-gray-300
               focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder></input>
+              " value={eventName}
+              onChange={handleEventNameChange}></input>
+              {errorMessage && (
+              <p class="text-red-500 text-xs italic">{errorMessage}</p>
+            )}
           </label>
 
-          <label class="eventrepeat">
-            <span class="text-gray-700 dark:text-gray-200">Does your event repeat?</span>
-            <select class="
+          <label className="eventDate">
+            <span className="text-gray-700 dark:text-gray-200">When is your Event?</span>
+            <input type="date" className="
+              p-2
+              mt-1
+              block
+              w-full
+              drop-shadow-md
+              rounded-md
+              dark:text-gray-700
+              bg-gray-200
+              border-gray-300
+              focus:border-gray-500 focus:bg-white focus:ring-0
+              "></input>
+          </label>
+
+          <label className="eventStartTime">
+            <span className="text-gray-700 dark:text-gray-200">What time does your event Start?</span>
+            <input type="time" className="
+              p-2
+              mt-1
+              block
+              w-full
+              drop-shadow-md
+              rounded-md
+              dark:text-gray-700
+              bg-gray-200
+              border-gray-300
+              focus:border-gray-500 focus:bg-white focus:ring-0
+              "></input>
+          </label>
+
+          <label className="eventEndTime">
+            <span className="text-gray-700 dark:text-gray-200">What time does your event end?</span>
+            <input type="time" className="
+              p-2
+              mt-1
+              block
+              w-full
+              rounded-md
+              drop-shadow-md
+              bg-gray-200
+              border-gray-300
+              dark:text-gray-700
+              focus:border-gray-500 focus:bg-white focus:ring-0
+              "></input>
+          </label>
+
+          <label className="eventLocation">
+            <span className="text-gray-700 dark:text-gray-200">Where will your event be held?</span>
+            <input type="text" className="
               p-2
               mt-1
               block
@@ -115,7 +113,23 @@ import { Link } from "react-router-dom";
               bg-gray-200
               border-gray-300
               focus:border-gray-500 focus:bg-white focus:ring-0
-              " placeholder='Location'>
+              "></input>
+          </label>
+
+          <label className="eventRepeat">
+            <span className="text-gray-700 dark:text-gray-200">Does your event repeat?</span>
+            <select className="
+              p-2
+              mt-1
+              block
+              w-full
+              rounded-md
+              drop-shadow-md
+              dark:text-gray-700
+              bg-gray-200
+              border-gray-300
+              focus:border-gray-500 focus:bg-white focus:ring-0
+              ">
                 <option></option>
                 <option>No</option>
                 <option>Daily</option>
@@ -124,9 +138,9 @@ import { Link } from "react-router-dom";
               </select>
           </label>
           
-          <label class="eventdetails">
-            <span class="text-gray-700 dark:text-gray-200">Additional Details(For you only):</span>
-            <textarea class="
+          <label className="eventDetails">
+            <span className="text-gray-700 dark:text-gray-200">Additional Details(For you only):</span>
+            <textarea className="
               
               mt-1
               block
@@ -137,7 +151,7 @@ import { Link } from "react-router-dom";
               bg-gray-200
               border-gray-300
               focus:border-gray-500 focus:bg-white focus:ring-0
-              " rows="5" spellcheck="true"></textarea>
+              "rows="5" spellCheck="true"></textarea>
           </label>
 
           <Button
@@ -146,14 +160,12 @@ import { Link } from "react-router-dom";
           text="Submit"
           borderRadius="10px"
           drop-shadow="md"
-          className="popup-button"/>
-
+          className="popup-button"
+          type="submit"/>
         </form>
       </main>
-
-
       </div>
-  )
-}
+  );
+};
 
 export default CreateEvent
